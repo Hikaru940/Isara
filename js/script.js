d59f9e1f5ad999cc5e90d91ca8190c0fd89cbc50
Hikaru Takasugi <gaoshanrijialiu@gmail.com>
2021-03-04 23:15:48 +0900

    add new file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(function() {
 
-
+    
+    
     if (window.matchMedia( "(max-width: 600px)" ).matches) {
+        $(".p-flow__feature-panel2").css("display", "none");
+        $("#js-flow-activate").addClass("open");
+        $("#js-flow-activate-first").css("display", "block");
         // $(".p-flow__feature-panel2").css("display", "none");
         $(".p-flow__feature-panel1").click(function () {
             $(".p-flow__feature-panel1").not(this).removeClass("open");
@@ -9,5 +13,15 @@ $(function() {
             $(this).toggleClass("open");
             $(this).next().slideToggle(200);
         });
-    }
+    };
+    
+    $(".p-faq__content__text").css("display", "none");
+    $("#js-faq-activate").addClass("open");
+    $("#js-faq-activate-first").css("display", "block");
+    
+    $(".p-faq__content__head").on('click', function () {
+        $(this).next().slideToggle(300);
+        $(this).toggleClass('open', 300);
+    });
+
 });
